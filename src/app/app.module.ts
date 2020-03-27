38fef24953d5af843ca03d7b2719cd3ad2abd8da
Manpreet <manpchop@cisco.com>
2020-03-28 01:05:23 +0530

    Angular Routing and Routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,46 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
+import { FeedbackComponent } from './feedback/feedback.component';
+import { NotesComponent } from './notes/notes.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import {Router, Routes, RouterModule} from "@angular/router";
+
+const appRoutes: Routes = [
+    {
+      path: 'notes',
+      component: NotesComponent
+    },
+    {
+      path: 'feedback',
+      component: FeedbackComponent
+    },
+    {
+      path: '',
+      component: NotesComponent,
+      pathMatch: 'full'
+    },
+    {
+      path: '**',
+      component: NotFoundComponent
+    }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationComponent
+    NavigationComponent,
+    FeedbackComponent,
+    NotesComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes, {enableTracing: true})
   ],
   providers: [],
   bootstrap: [AppComponent]
